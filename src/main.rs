5c9076cc9a2659821f107a9c5df2fbe126804fd8
Victor Tortolero <victormtortolero@gmail.com>
2023-04-21 23:02:00 -0600

    add support for PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+// Uncomment this block to pass the first stagE
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+enum MessageType {
+    SimpleString,
+    Error,
+    Integer,
+    BulkString,
+    Array,
+}
+
+fn format_message(kind: MessageType, body: String) -> String {
+    let kind_string = match kind {
+        MessageType::SimpleString => "+".to_string(),
+        MessageType::Error => "-".to_string(),
+        MessageType::Integer => ":".to_string(),
+        MessageType::BulkString => "$".to_string(),
+        MessageType::Array => "*".to_string(),
+    };
+    format!("{}{}\r\n", kind_string, body)
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut incoming_stream) => {
                 println!("accepted new connection");
+                incoming_stream
+                    .write(format_message(MessageType::SimpleString, "PONG".to_string()).as_bytes())
+                    .unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
